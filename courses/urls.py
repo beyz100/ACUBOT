3f2b6f092adde70e28355cf729800d7de83d4285
Mirket07 <cerenyurdunol@gmail.com>
2026-03-14 11:56:26 +0300

    added API endpoints and serializers for Faculty, Department, and Course models

---
+++ courses/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import FacultyViewSet, DepartmentViewSet, CourseViewSet
+
+router = DefaultRouter()
+router.register(r'faculties', FacultyViewSet)
+router.register(r'departments', DepartmentViewSet)
+router.register(r'courses', CourseViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
